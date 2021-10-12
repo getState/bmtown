299d752db9e2917956f4671d61d0cdcc1c277e04
getState <asj446@naver.com>
2021-10-12 11:47:14 +0900

    chore: [FE] frontend 초기 환경설정
    - CRA를 이용하여 초기 환경설정 완료
    - App.test.js같은 불필요한 파일 삭제

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter ,Route } from "react-router-dom";
+import { Home, About } from "./pages/index.js";
+
+export default function App(){
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </BrowserRouter>
+    );
+}
