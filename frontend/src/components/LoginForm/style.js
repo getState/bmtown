2dd26f056d3fd5561e6d1ff0dd43461ceab46ec0
kimsungho <kimsungho97@gmail.com>
2021-10-12 18:15:28 +0900

    fix: LoginForm style 수정
    background-color 및 크기 등 디자인 수정

--- frontend/src/components/LoginForm/style.js
+++ frontend/src/components/LoginForm/style.js
@@ -11,19 +11,40 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
-    width: 300px;
-    height: 100px;
-    border-radius: 10px;
+    width: 200px;
+    height: 50px;
+    margin-bottom: 30px;
+    border: 1px solid #D7D7D7;
+    border-radius: 8px;
+    background: #FFFFFF;
     &:focus{
-        outline: 1px solid blue;
+        outline: none;
+        border: 1px solid #045D8B;
     }
 `
 
-const ButtonCommon=(bgColor)=>`
-    width: 300px;
-    height: 100px;
-    background-color: ${bgColor}
+const ButtonCommon = (bgColor) =>`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 50px;
+    margin-top: 20px;
+    
+    background-color: ${bgColor};
     border-radius: 30px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 14px;
+    color: #FFFFFF;
+    border: 0;
+
+    &:hover{
+      cursor: pointer;
+    }
 `
 
 export const LoginButton = styled.button`
