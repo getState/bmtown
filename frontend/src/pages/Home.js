3b3d4328475b60a52eddaff16f044ea1e5319bf0
getState <asj446@naver.com>
2021-10-12 17:18:21 +0900

    feat: [FE] pages 골격 생성
    Home(로그인)등

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
+import LoginContainer from "../components/LoginContainer";
 
 const Home = () => {
   return (
-    <div>
-      <h2>홈</h2>
-    </div>
+    <LoginContainer></LoginContainer>
   );
 };
 
