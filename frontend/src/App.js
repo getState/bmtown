00dc25d3a49e02040b16b48f4b3431c1dc6efe5a
kimsungho <kimsungho97@gmail.com>
2021-10-12 18:17:39 +0900

    feat: reset.css 파일 삽입 및 적용
    reset.css파일 삽입 및 적용 진행

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter ,Route } from "react-router-dom";
 import { Home, About } from "./pages/index.js";
+import './reset.css';
 
 export default function App(){
-    return (
+  return (
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
