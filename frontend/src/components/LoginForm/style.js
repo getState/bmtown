19ba8db6c9bdaf290a29d5333d80056c98620af9
getState <asj446@naver.com>
2021-10-13 19:14:35 +0900

    fix: [FE] 로그인 폼 디자인 수정

--- frontend/src/components/LoginForm/style.js
+++ frontend/src/components/LoginForm/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import {Link} from "react-router-dom";
 
 export const Form = styled.form`
     width: 500px;
@@ -17,9 +17,13 @@ export const Input = styled.input`
     border: 1px solid #D7D7D7;
     border-radius: 8px;
     background: #FFFFFF;
+    font-size: 16px;
+    padding: 0px 10px;
+    transition: 0.3s all ease-in;
     &:focus{
         outline: none;
         border: 1px solid #045D8B;
+        transform: scale(1.1);
     }
 `
 
@@ -36,21 +40,22 @@ const ButtonCommon = (bgColor) =>`
     border-radius: 30px;
     font-family: Roboto;
     font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
+    font-weight: 600;
+    font-size: 14px;
     line-height: 14px;
     color: #FFFFFF;
     border: 0;
-
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    transition: 0.3s all ease-in;
     &:hover{
       cursor: pointer;
+      transform: scale(1.1);
     }
 `
 
 export const LoginButton = styled.button`
-  ${ButtonCommon("blue")}
+  ${ButtonCommon("#045D8B")}
 `;
-
-export const SignupButton = styled.a`
+export const SignupButton = styled(Link)`
   ${ButtonCommon("grey")}
 `;
