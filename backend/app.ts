975054defa71575968a5c4508cb6fdf963941a79
kimsungho <kimsungho97@gmail.com>
2021-10-14 17:46:55 +0900

    feat: [BE]userRouter추가, 테스트케이스 삭제, cors호스트 추가
    - user 로그인, 회원가입 위한 라우터 추가
    - 기존 테스트케이스 제거
    - cors -> localhost 추가

--- backend/app.ts
+++ backend/app.ts
@@ -2,12 +2,13 @@ import * as express from "express"
 import { connect } from "./db";
 import { User } from "./models/User";
 import authRouter from "./routes/auth";
+import userRouter from "./routes/user";
 const cors=require("cors");
 
 connect();
 
 const corsOption = {
-  origin: "http://127.0.0.1:3000",
+  origin: ["http://127.0.0.1:3000","http://localhost:3000"],
   credentials: true
 }
 
@@ -23,26 +24,8 @@ const app = new App().application;
 app.use(express.json());
 app.use(cors(corsOption));
 
-app.get("/add", async (req: express.Request, res: express.Response) => {
-  const user = new User({
-    username: "testuser",
-    email: "test@test.com",
-    password: "testpassword"
-  });
-  user.save((err, userInfo) => {
-    console.log("Added user" + JSON.stringify(user))
-    res.send("Add user complete");
-  }); 
-})
 
-app.get("/", async (req: express.Request, res: express.Response) => {
-  User.find({}, (err, user) => {
-    console.log("get User from Mongo");
-    res.send(user);
-  }).clone().catch(function (err) { console.log(err) });
-  
-})
-
+app.use("/user", userRouter);
 app.use("/auth", authRouter);
 
 app.listen(5000,()=>console.log("start"));
