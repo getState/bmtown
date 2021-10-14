f93ef4ed081920514841ffe23d3bc9314af9942c
kimsungho <kimsungho97@gmail.com>
2021-10-14 17:56:22 +0900

    feat: [FE] 로그인, 회원가입 로직 구현 - 콘솔출력 확인 가능

--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import { Form, Input, LoginButton, SignupButton} from './style';
   
 
@@ -8,21 +9,37 @@ export default function LoginForm() {
     const callbackURL = `http://127.0.0.1:3000/auth/callback`;
     const url = `https://github.com/login/oauth/authorize?client_id=${cliendId}&redirect_url=${callbackURL}`;
 
+    const [userId, setUserId] = useState('');
+
     return (
-        <Form>
+        <div>
             <Input
                 type="text"
                 placeholder="아이디를 입력하세요."
+                onChange={(e)=>setUserId(e.target.value)}
                 required
             />
             
-            <LoginButton>
+            <LoginButton
+                onClick={()=>{doLogin(userId)}}
+            >
                 로그인
             </LoginButton>
 
             <SignupButton href={url}>
                 회원가입
             </SignupButton>
-        </Form>
+        </div>
     );
+}
+
+const doLogin =async  (userId) => {
+    const { userInfo, token } = (await axios.get(`http://127.0.0.1:5000/user/?userId=${userId}`)).data;
+     
+    if (userInfo !== null) {
+        console.log("로그인 성공!");
+    }
+    else {
+        console.log("로그인 실패!");
+    }
 }
--- frontend/src/components/RegisterForm/index.js
+++ frontend/src/components/RegisterForm/index.js
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 
 export default function RegisterForm() {
     const [accessToken, setAccessToken] = useState(undefined);
+    const [userId, setUserId] = useState('');
+    const [nickname, setNickname] = useState('');
 
     const searchParams = new URLSearchParams(useLocation().search);
     const code = searchParams.get("code");
@@ -15,16 +17,13 @@ export default function RegisterForm() {
         const { data } = await axios.post("http://127.0.0.1:5000/auth", { code });
         const { accessToken, userInfomation } = data;
         setAccessToken(accessToken);
-        console.log(accessToken);
     }
-
-
     useEffect(() => {
         getAccessToken(code);
-    },[])
+    }, [])
 
     return (
-        <Form>
+        <div>
             <Label>
                 <Text>
                     아이디
@@ -32,6 +31,7 @@ export default function RegisterForm() {
                 <InputId
                     type="text"
                     placeholder="아이디를 입력하세요."
+                    onChange={(e) => setUserId(e.target.value)}
                     required
                 >
                 </InputId>
@@ -43,15 +43,29 @@ export default function RegisterForm() {
                 <InputNick
                     type="text"
                     placeholder="타운에서 사용될 닉네임을 입력하세요"
+                    onChange={(e) => setNickname(e.target.value)}
                     required
                 >
                 </InputNick>
             </Label>
-            <Button>
+            <Button
+                onClick={() => doRegister(accessToken, userId, nickname)}
+            >
                 회원가입
             </Button>
-        </Form>
-
+        </div>
     );
 }
 
+
+
+const doRegister = async (accessToken, userId, nickname) => {
+    const { result } = (await axios.post("http://127.0.0.1:5000/user", { accessToken, userId, nickname })).data;
+    console.log(result);
+    if (result) {
+        window.location.href = "/";
+    }
+    else {
+        alert("이미 존재하는 id이거나 이미 회원가입된 계정입니다.");
+    }
+}
