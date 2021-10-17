9a36db285232c1af5a0f37d16bd5409e28d41f87
kimsungho <kimsungho97@gmail.com>
2021-10-18 00:38:45 +0900

    fix: [FE] 로고 이미지 경로 수정

--- frontend/src/components/LoginContainer/index.js
+++ frontend/src/components/LoginContainer/index.js
@@ -9,7 +9,7 @@ export default function LoginContainer(){
     return (
         <Container>
             <LogoLink to="/">
-                <LogoImg src="logo.png" />
+                <LogoImg src="/logo.png" />
             </LogoLink>
             <Route exact path="/" component={LoginForm}></Route>
             <Route exact path="/auth/callback" component={RegisterForm}></Route>
