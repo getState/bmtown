b81b60337eeadeed046c5a5c07225a79ae451dcc
getState <asj446@naver.com>
2021-10-18 10:46:50 +0900

    feat: [FE] chat component 구현

---
+++ frontend/src/components/Chat/style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 180px;
+    height: 100vh;
+    min-height: 300px;
+    padding: 5px 10px 10px 10px;
+    background-color: #242544;
+    
+`;
+
+export const ChatHead = styled.div`
+`;
+
+export const HeadText = styled.div`
+    float: left;
+    font-size: 20px;
+    color: white;
+    font-weight: 600;
+    
+`
+
+export const BackButton = styled.button`
+    float: right;
+    font-size: 20px;
+    color: white;
+    font-weight: 600;
+    background-color: #242544;
+    border: none;
+    cursor: pointer;
+`
+
+export const MessageContainer = styled.div`
+    flex-grow: 1;
+`
+
+export const Message = styled.div`
+    margin-top: 10px;
+    background-color: #333a64;
+`
+
+export const ChatInput = styled.input`
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #333a64;
+    border-radius: 20px;
+    background-color: 242544;
+`
