798f1df60061902f2c8799e59a7a108d518b764d
kimsungho <kimsungho97@gmail.com>
2021-10-19 20:58:21 +0900

    feat: [FE] Town페이지 jsx 변경
    - Div태그로 묶음

--- frontend/src/pages/Town.js
+++ frontend/src/pages/Town.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import Chat from "../components/Chat";
+import TownMain from "../components/TownMain";
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 const Town = () => {
   return (
-    <Chat />
+    <Div>
+      <Chat/>
+      <TownMain/>
+    </Div>
   );
 };
 
