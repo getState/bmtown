024078ca489752c1e88cb2bbccfa2392ca749a25
kimsungho <kimsungho97@gmail.com>
2021-10-21 02:34:36 +0900

    fix: [FE] 필요없는 import 제거

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
-import { userAtom } from '../store/user';
 
 export const fetchLogin =async  (userId, setUser, history) => {
     const { userInfo, token } = (await axios.get(`http://127.0.0.1:5000/user/?userId=${userId}`)).data;
-    console.log(userInfo);
     if (userInfo !== null) {
         history.push("/town");
-        console.log(userInfo);
         setUser({nickname: userInfo.nickname, userId: userInfo.userId});
-        
         return true;
     }
     else {
